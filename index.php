11e89adb19187258a9ac1ae81170d12476a214f9
SingleDeveloper110 <mohammadmatindeveloper110@gmail.com>
2026-05-26 08:51:31 +0330

    Install Tailwind & Insert Tailwind CLI build process

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./assets/css/output.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="flex justify-center items-center py-5">
+        <h1 class="text-7xl text-white bg-blue-800 py-2 px-2">Mini-CMS</h1>
+    </div>
+</body>
+
+</html>
