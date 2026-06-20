e625162c09661c9d12ccbd7a3df2ccc4996218cf
SingleDeveloper110 <mohammadmatindeveloper110@gmail.com>
2026-06-20 14:11:43 +0330

    Fixed somethings

--- dashboard.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("config.php");
-include_page_verify("create_post", "true", "./assets/partition/create_post.php",false);
-include('./assets/partition/header_dashboard.php');
-echo "DASHBORAD HERE";
